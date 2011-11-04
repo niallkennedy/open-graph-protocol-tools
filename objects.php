69fc10e4235ede42ea7f02583e003617bd8bc9c2
Niall Kennedy <niall@niallkennedy.com>
2011-11-04 16:37:28 -0700

    global object support. support string arrays. move media and global objects into their own files

---
+++ objects.php
@@ -0,0 +1,321 @@
+<?php
+/**
+ * Open Graph protocol global types
+ *
+ * @link http://ogp.me/#types Open Graph protocol global types
+ * @package open-graph-protocol-tools
+ * @author Niall Kennedy <niall@niallkennedy.com>
+ * @version 1.3
+ * @copyright Public Domain
+ */
+if ( !class_exists('OpenGraphProtocol') )
+	require_once dirname(__FILE__) . '/open-graph-protocol.php';
+
+abstract class OpenGraphProtocolObject {
+	const prefix ='';
+	const NS='';
+
+	/**
+	 * Output the object as HTML <meta> elements
+	 * @return string HTML meta element string
+	 */
+	public function toHTML() {
+		return rtrim( OpenGraphProtocol::buildHTML( get_object_vars($this), static::prefix ), PHP_EOL );
+	}
+}
+
+class OpenGraphProtocolArticle extends OpenGraphProtocolObject {
+	/**
+	 * Property prefix
+	 * @var string
+	 */
+	const prefix = 'article';
+
+	/**
+	 * prefix namespace
+	 * @var string
+	 */
+	const NS = 'http://ogp.me/ns/article#';
+
+	/**
+	 * When the article was first published.
+	 * ISO 8601 formatted string.
+	 * @var string
+	 */
+	protected $published_time;
+
+	/**
+	 * When the article was last changed
+	 * ISO 8601 formatted string.
+	 * @var string
+	 */
+	protected $modified_time;
+
+	/**
+	 * When the article is considered out-of-date
+	 * ISO 8601 formatted string.
+	 * @var string
+	 */
+	protected $expiration_time;
+
+	/**
+	 * Writers of the article.
+	 * Array of author URIs
+	 * @var array
+	 */
+	protected $author;
+
+	/**
+	 * High-level section or category
+	 * @var string
+	 */
+	protected $section;
+
+	/**
+	 * Content tag
+	 * Array of tag strings
+	 * @var array
+	 */
+	protected $tag;
+
+	/**
+	 * Initialize arrays
+	 */
+	public function __construct() {
+		$this->author = array();
+		$this->tag = array();
+	}
+
+	/**
+	 * Convert a DateTime object to GMT and format as an ISO 8601 string.
+	 * @param DateTime $date date to convert
+	 * @return string ISO 8601 formatted datetime string
+	 */
+	public static function datetime_to_iso_8601( DateTime $date ) {
+		$date->setTimezone(new DateTimeZone('GMT'));
+		return $date->format('c');
+	}
+
+	/**
+	 * When the article was first published
+	 * @return string ISO 8601 formatted publication date and optional time
+	 */
+	public function getPublishedTime() {
+		return $this->published_time;
+	}
+
+	/**
+	 * Set when the article was first published
+	 * @param DateTime|string $pubdate ISO 8601 formatted datetime string or DateTime object for conversion
+	 */
+	public function setPublishedTime( $pubdate ) {
+		if ( $pubdate instanceof DateTime )
+			$this->published_time = static::datetime_to_iso_8601($pubdate);
+		else if ( is_string($pubdate) && strlen($pubdate) >= 10 ) // at least YYYY-MM-DD
+			$this->published_time = $pubdate;
+		return $this;
+	}
+
+	/**
+	 * When article was last changed
+	 * @return string ISO 8601 formatted modified date and optional time
+	 */
+	public function getModifiedTime() {
+		return $this->modified_time;
+	}
+
+	/**
+	 * Set when the article was last changed
+	 * @param DateTime|string $updated ISO 8601 formatted datetime string or DateTime object for conversion
+	 */
+	public function setModifiedTime( $updated ) {
+		if ( $updated instanceof DateTime )
+			$this->modified_time = static::datetime_to_iso_8601($updated);
+		else if ( is_string($updated) && strlen($updated) >= 10 ) // at least YYYY-MM-DD
+			$this->modified_time = $updated;
+		return $this;
+	}
+
+	/**
+	 * Time the article content expires
+	 * @return string ISO 8601 formatted expiration date and optional time
+	 */
+	public function getExpirationTime() {
+		return $this->expiration_time;
+	}
+
+	/**
+	 * Set when the article content expires
+	 * @param DateTime|string $expires ISO formatted datetime string or DateTime object for conversion
+	 */
+	public function setExpirationTime( $expires ) {
+		if ( $expires instanceof DateTime )
+			$this->expiration_time = static::datetime_to_iso_8601($expires);
+		else if ( is_string($expires) && strlen($expires) >= 10 )
+			$this->expiration_time = $expires;
+		return $this;
+	}
+
+	/**
+	 * Article author URIs
+	 * @return array Article author URIs
+	 */
+	public function getAuthors() {
+		return $this->author;
+	}
+
+	/**
+	 * Add an author URI
+	 * @param string $author_uri Author URI
+	 */
+	public function addAuthor( $author_uri ) {
+		if ( is_string($author_uri) && !in_array($author_uri, $this->author))
+			$this->author[] = $author_uri;
+		return $this;
+	}
+
+	/**
+	 * High-level section name
+	 */
+	public function getSection() {
+		return $this->section();
+	}
+
+	/**
+	 * Set the top-level content section
+	 * @param string $section
+	 */
+	public function setSection( $section ) {
+		if ( is_string($section) && !empty($section) )
+			$this->section = $section;
+		return $this;
+	}
+
+	/**
+	 * Content tags
+	 * @return array content tags
+	 */
+	public function getTags() {
+		return $this->tag;
+	}
+
+	/**
+	 * Add a content tag
+	 * @param string $tag content tag
+	 */
+	public function addTag( $tag ) {
+		if ( is_string($tag) && !empty($tag) )
+			$this->tag[] = $tag;
+		return $this;
+	}
+}
+
+class OpenGraphProtocolProfile extends OpenGraphProtocolObject {
+	/**
+	 * Property prefix
+	 * @var string
+	 */
+	const prefix = 'profile';
+
+	/**
+	 * prefix namespace
+	 * @var string
+	 */
+	const NS = 'http://ogp.me/ns/profile#';
+
+	/**
+	 * A person's given name
+	 * @var string
+	 */
+	protected $first_name;
+
+	/**
+	 * A person's last name
+	 * @var string
+	 */
+	protected $last_name;
+
+	/**
+	 * The profile's unique username
+	 * @var string
+	 */
+	protected $username;
+
+	/**
+	 * Gender: male or female
+	 */
+	protected $gender;
+
+	/**
+	 * Get the person's given name
+	 * @return string given name
+	 */
+	public function getFirstName() {
+		return $this->first_name;
+	}
+
+	/**
+	 * Set the person's given name
+	 * @param string $first_name given name
+	 */
+	public function setFirstName( $first_name ) {
+		if ( is_string($first_name) && !empty($first_name) )
+			$this->first_name = $first_name;
+		return $this;
+	}
+
+	/**
+	 * The person's family name
+	 * @return string famil name
+	 */
+	public function getLastName() {
+		return $this->last_name;
+	}
+
+	/**
+	 * Set the person's family name
+	 * @param string $last_name family name
+	 */
+	public function setLastName( $last_name ) {
+		if ( is_string($last_name) && !empty($last_name) )
+			$this->last_name = $last_name;
+		return $this;
+	}
+
+	/**
+	 * Person's username on your site
+	 * @return string account username
+	 */
+	public function getUsername() {
+		return $this->username;
+	}
+
+	/**
+	 * Set the account username
+	 * @param string $username username
+	 */
+	public function setUsername( $username ) {
+		if ( is_string($username) && !empty($username) )
+			$this->username = $username;
+		return $this;
+	}
+
+	/**
+	 * The person's gender. male|female
+	 * @return string male|female
+	 */
+	public function getGender() {
+		return $this->gender;
+	}
+
+	/**
+	 * Set the person's gender
+	 * @param string $gender male|female
+	 */
+	public function setGender( $gender ) {
+		if ( is_string($gender) && ( $gender === 'male' || $gender === 'female' ) )
+			$this->gender = $gender;
+		return $this;
+	}
+}
+?>
--- open-graph-protocol.php
+++ open-graph-protocol.php
@@ -44,6 +44,18 @@ class OpenGraphProtocol {
     const META_ATTR = 'property';
 
 	/**
+	 * Property prefix
+	 * @var string
+	 */
+	const PREFIX = 'og';
+
+	/**
+	 * prefix namespace
+	 * @var string
+	 */
+	const NS = 'http://ogp.me/ns#';
+
+	/**
 	 * Page classification according to a pre-defined set of base types.
 	 *
 	 * @var string
@@ -128,7 +140,7 @@ class OpenGraphProtocol {
 	 * @param array $og associative array of OGP properties and values
 	 * @param string $prefix optional prefix to prepend to all properties
 	 */
-	public static function buildHTML( array $og, $prefix='og' ) {
+	public static function buildHTML( array $og, $prefix=self::PREFIX ) {
 		if ( empty($og) )
 			return;
 
@@ -137,13 +149,15 @@ class OpenGraphProtocol {
 			if ( is_object( $content ) || is_array( $content ) ) {
 				if ( is_object( $content ) )
 					$content = $content->toArray();
-				$newprefix = $prefix;
 				if ( empty($property) )
 					$s .= static::buildHTML( $content, $prefix );
 				else
 					$s .= static::buildHTML( $content, $prefix . ':' . $property );
-			} elseif ( !empty($property) && !empty($content) ) {
-				$s .= '<meta ' . self::META_ATTR . '="' . $prefix . ':' . htmlspecialchars( $property ) . '" content="' . htmlspecialchars($content) . '">' . PHP_EOL;
+			} elseif ( !empty($content) ) {
+				$s .= '<meta ' . self::META_ATTR . '="' . $prefix;
+				if ( !empty($property) )
+					$s .= ':' . htmlspecialchars( $property );
+				$s .= '" content="' . htmlspecialchars($content) . '">' . PHP_EOL;
 			}
 		}
 		return $s;
@@ -483,265 +497,6 @@ class OpenGraphProtocol {
 	}
 }
 
-/**
- * Describe a media object
- *
- * @version 1.3
- */
-abstract class OpenGraphProtocolMedia {
-
-	/**
-	 * HTTP scheme URL
-	 *
-	 * @var string
-	 * @since 1.3
-	 */
-	protected $url;
-
-	/**
-	 * HTTPS scheme URL
-	 *
-	 * @var string
-	 * @since 1.3
-	 */
-	protected $secure_url;
-
-	/**
-	 * Internet media type of the linked URLs
-	 *
-	 * @var string
-	 * @since 1.3
-	 */
-	protected $type;
-
-	public function toArray() {
-		return get_object_vars($this);
-	}
-
-	/**
-	 * @return string URL string or null if not set
-	 */
-    public function getURL() {
-    	return $this->url;
-    }
-
-	/**
-	 * Set the media URL
-	 *
-	 * @param string $url resource location
-	 */
-	public function setURL( $url ) {
-		if ( is_string( $url ) && !empty( $url ) ) {
-			$url = trim($url);
-			if (OpenGraphProtocol::VERIFY_URLS) {
-				$url = OpenGraphProtocol::is_valid_url( $url, array( 'text/html', 'application/xhtml+xml' ) );
-			}
-			if (!empty($url))
-				$this->url = $url;
-		}
-		return $this;
-	}
-
-	/**
-	 * @return string secure URL string or null if not set
-	 */
-	public function getSecureURL() {
-		return $this->url;
-	}
-
-	/**
-	 * Set the secure URL for display in a HTTPS page
-	 *
-	 * @param string $url resource location
-	 */
-	public function setSecureURL( $url ) {
-		if ( is_string( $url ) && !empty( $url ) ) {
-			$url = trim($url);
-			if (OpenGraphProtocol::VERIFY_URLS) {
-				if ( parse_url($url,PHP_URL_SCHEME) === 'https' ) {
-					$url = OpenGraphProtocol::is_valid_url( $url, array( 'text/html', 'application/xhtml+xml' ) );
-				} else {
-					$url = '';
-				}
-			}
-			if (!empty($url))
-				$this->secure_url = $url;
-		}
-		return $this;
-	}
-
-	/**
-	 * Get the Internet media type of the referenced resource
-	 *
-	 * @return string Internet media type or null if none set
-	 */
-	public function getType() {
-		return $this->type;
-	}
-}
-
-abstract class OpenGraphProtocolVisualMedia extends OpenGraphProtocolMedia {
-	/**
-	 * Height of the media object in pixels
-	 *
-	 * @var int
-	 * @since 1.3
-	 */
-	protected $height;
-
-	/**
-	 * Width of the media object in pixels
-	 *
-	 * @var int
-	 * @since 1.3
-	 */
-	protected $width;
-
-    	/**
-	 * @return int width in pixels
-	 */
-	public function getWidth() {
-		return $this->width;
-	}
-
-	/**
-	 * Set the object width
-	 *
-	 * @param int $width width in pixels
-	 */
-	public function setWidth( $width ) {
-		if ( is_int($width) && $width >  0 )
-			$this->width = $width;
-		return $this;
-	}
-
-	/**
-	 * @return int height in pixels
-	 */
-	public function getHeight() {
-		return $this->height;
-	}
-
-	/**
-	 * Set the height of the referenced object in pixels
-	 * @var int height of the referenced object in pixels
-	 */
-	public function setHeight( $height ) {
-		if ( is_int($height) && $height > 0 )
-			$this->height = $height;
-		return $this;
-	}
-}
-
-/**
- * An image representing page content. Suitable for display alongside a summary of the webpage.
- */
-class OpenGraphProtocolImage extends OpenGraphProtocolVisualMedia {
-	/**
-	 * Map a file extension to a registered Internet media type
-	 *
-	 * @link http://www.iana.org/assignments/media-types/image/index.html IANA image types
-	 * @param string $extension file extension
-	 * @return string Internet media type in the format image/* 
-	 */
-	public static function extension_to_media_type( $extension ) {
-		if ( empty($extension) || ! is_string($extension) )
-			return;
-		if ( $extension === 'jpeg' || $extension === 'jpg' )
-			return 'image/jpeg';
-		else if ( $extension === 'png' )
-			return 'image/png';
-		else if ( $extension === 'gif' )
-			return 'image/gif';
-		else if ( $extension === 'svg' )
-			return 'image/svg+sml';
-		else if ( $extension === 'ico' )
-			return 'image/vnd.microsoft.icon';
-	}
-
-	/**
-	 * Set the Internet media type. Allow only image types.
-	 *
-	 * @param string $type Internet media type
-	 */
-	public function setType( $type ) {
-		if ( substr_compare( $type, 'image/', 0, 6 ) === 0 )
-			$this->type = $type;
-		return $this;
-	}
-}
-
-/**
- * A video that complements the webpage content
- */
-class OpenGraphProtocolVideo extends OpenGraphProtocolVisualMedia {
-	/**
-	 * Map a file extension to a registered Internet media type
-	 * Include Flash as a video type due to its popularity as a wrapper
-	 *
-	 * @link http://www.iana.org/assignments/media-types/video/index.html IANA video types
-	 * @param string $extension file extension
-	 * @return string Internet media type in the format video/* or Flash
-	 */
-	public static function extension_to_media_type( $extension ) {
-		if ( empty($extension) || ! is_string($extension) )
-			return;
-		if ( $extension === 'swf' )
-			return 'application/x-shockwave-flash';
-		else if ( $extension === 'mp4' )
-			return 'video/mp4';
-		else if ( $extension === 'ogv' )
-			return 'video/ogg';
-		else if ( $extension === 'webm' )
-			return 'video/webm';
-	}
-
-	/**
-	 * Set the Internet media type. Allow only video types + Flash wrapper.
-	 *
-	 * @param string $type Internet media type
-	 */
-	public function setType( $type ) {
-		if ( $type === 'application/x-shockwave-flash' || substr_compare( $type, 'video/', 0, 6 ) === 0 )
-			$this->type = $type;
-		return $this;
-	}
-}
-
-/**
- * Audio file suitable for playback alongside the main linked content
- */
-class OpenGraphProtocolAudio extends OpenGraphProtocolMedia {
-	/**
-	 * Map a file extension to a registered Internet media type
-	 * Include Flash as a video type due to its popularity as a wrapper
-	 *
-	 * @link http://www.iana.org/assignments/media-types/audio/index.html IANA video types
-	 * @param string $extension file extension
-	 * @return string Internet media type in the format audio/* or Flash
-	 */
-	public static function extension_to_media_type( $extension ) {
-		if ( empty($extension) || ! is_string($extension) )
-			return;
-		if ( $extension === 'swf' )
-			return 'application/x-shockwave-flash';
-		else if ( $extension === 'mp3' )
-			return 'audio/mpeg';
-		else if ( $extension === 'm4a' )
-			return 'audio/mp4';
-		else if ( $extension === 'ogg' || $extension === 'oga' )
-			return 'audio/ogg';
-	}
-
-	/**
-	 * Set the Internet media type. Allow only audio types + Flash wrapper.
-	 *
-	 * @param string $type Internet media type
-	 */
-	public function setType( $type ) {
-		if ( $type === 'application/x-shockwave-flash' || substr_compare( $type, 'audio/', 0, 6 ) === 0 )
-			$this->type = $type;
-		return $this;
-	}
-}
+include_once dirname(__FILE__) . '/media.php';  // image, video, audio
+include_once dirname(__FILE__) . '/objects.php'; // global objects: profile, article
 ?>
